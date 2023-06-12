1ad4e2df1dc5255c29453c68e6aa9ed37381becb
Caio H Perlin <caiohperlin@gmail.com>
2023-06-11 21:18:25 -0400

    Select e insert adicionados (Get/Post)

---
+++ db.js
@@ -0,0 +1,22 @@
+const { Pool } = require('pg');
+
+let connect = async function () {
+  try {
+    if (global.connection) {
+      return Promise.resolve(global.connection);
+    }
+
+    const pool = new Pool({
+      //protocolo://usuario:senha@localdobanco/nomedobanco
+      connectionString: 'postgres://postgres:postgres@localhost/mc_centurion'
+    });
+
+    global.connection = pool;
+    return Promise.resolve(pool);
+  } catch (error) {
+    console.error('Erro ao estabelecer a conexão:', error);
+    throw error;
+  }
+};
+
+module.exports = { connect };
